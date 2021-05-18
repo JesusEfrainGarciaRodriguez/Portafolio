2b5c4e79a54e61559a38b7466efbae988cb875ed
Efrain Garcia <66291025+JesusEfrainGarciaRodriguez@users.noreply.github.com>
2021-05-18 11:21:32 -0600

    Actualización de trabajos
    +Se agregaron modales para mostrar un video del trabajo
    +Se acomodaron algunos estilos en carpetas

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function abrirModal(id) {
+  let modal = document.getElementById(id);
+  modal.style.display = "block";
+}
+
+function cerrarModal(id) {
+  let modal = document.getElementById(id);
+  modal.style.display = "none";
+}
