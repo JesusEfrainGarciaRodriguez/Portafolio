7ff3a717fbcf2019ddccb35cd828583cf3902e22
Efrain Garcia <66291025+JesusEfrainGarciaRodriguez@users.noreply.github.com>
2021-05-24 13:37:29 -0600

    Modificación modals
    +Se cambio la forma de trabajar de los modal, ahora toda la lógica esta en el archivo js y en el html solo es necesario especificar el data-target.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
-function abrirModal(id) {
-  let modal = document.getElementById(id);
-  modal.style.display = "block";
+// Modales
+function getAllElementsWithAttribute(attribute)
+{
+  // Arreglo con los elementos que buscamos
+  var matchingElements = [];
+  // Se obtienen todos los elemtos del document
+  var allElements = document.getElementsByTagName('*');
+  for (var i = 0, n = allElements.length; i < n; i++)
+  {
+    if (allElements[i].getAttribute(attribute) !== null)
+    {
+      // Elementos existentes con el atributo se agregan al arreglo
+      matchingElements.push(allElements[i]);
+    }
+  }
+  return matchingElements;
 }
 
-function cerrarModal(id) {
-  let modal = document.getElementById(id);
-  modal.style.display = "none";
+var btnModal = getAllElementsWithAttribute('data-target');
+
+for(var x=0; x < btnModal.length; x++) {
+  btnModal[x].addEventListener('click', modalToggle);
+}
+
+function modalToggle() {
+  var id = this.getAttribute('data-target');
+  var modal = document.getElementById(id);
+  modal.classList.toggle('hide');
 }
