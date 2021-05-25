174f66e5322dfea21ad2566e7b1b5033fbc22770
Efrain Garcia <66291025+JesusEfrainGarciaRodriguez@users.noreply.github.com>
2021-05-25 10:50:21 -0600

    Actualización de modals
    + Al clickear fuera de un modal se cierra

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Modales
+// Obtener todos los elementos con un atributo a especificar
 function getAllElementsWithAttribute(attribute)
 {
   // Arreglo con los elementos que buscamos
@@ -16,14 +16,27 @@ function getAllElementsWithAttribute(attribute)
   return matchingElements;
 }
 
+//-------------------- Modales --------------------------------------
 var btnModal = getAllElementsWithAttribute('data-target');
 
+// Asignar evento a todos los elementos de abrir o cerrar modals 
 for(var x=0; x < btnModal.length; x++) {
   btnModal[x].addEventListener('click', modalToggle);
 }
 
+// Ocultar o mostrar modals
 function modalToggle() {
   var id = this.getAttribute('data-target');
   var modal = document.getElementById(id);
   modal.classList.toggle('hide');
 }
+
+// Cerrar modals al clickear fuera
+document.addEventListener("click", e => {
+  modal = document.getElementsByClassName('modal');
+  for(i=0; i < modal.length; i++){
+    if (e.target == modal[i]) {
+      modal[i].classList.toggle('hide');
+    }
+  }
+});
