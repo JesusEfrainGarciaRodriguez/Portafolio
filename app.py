9412cfaa3f95ce1380b37e47468ea03e7119199e
Efrain Garcia <66291025+JesusEfrainGarciaRodriguez@users.noreply.github.com>
2021-05-27 10:21:04 -0600

    Implentación de backend
    + Se adapto el proyecto para trabajar con Flask
    + Ahora es posible enviar correos electrónicos desde el formulario

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'portafolio.email@gmail.com'
+app.config['MAIL_PASSWORD'] = '#Portafolio1234'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/enviar_correo', methods=['POST'])
+def enviar_correo():
+    if(request.method == 'POST'):
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+        msg = request.form['mensaje']
+
+        message = Message('Mensaje de portafolio', sender='portafolio.email@gmail.com',
+              recipients=["efraingarcia_86@hotmail.com"])
+
+        message.body = """Mensaje de: %s, con el correo: %s.\n\n%s""" % (nombre, correo, msg,)
+        mail.send(message)
+
+        return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)
